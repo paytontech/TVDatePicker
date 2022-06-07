73ca43802b3de812ea39577e33db3c2203dd146a
paytondev <81662879+paytontech@users.noreply.github.com>
2022-06-06 22:52:20 -0400

    I love Swift development! (crying)

--- Package.swift
+++ Package.swift
@@ -4,25 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "TVDatePickerDev",
+    name: "TVDatePicker",
     platforms: [
         .tvOS(.v16)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "TVDatePickerDev",
-            targets: ["TVDatePickerDev"]),
+            name: "TVDatePicker",
+            targets: ["TVDatePicker"]),
     ],
     dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "TVDatePickerDev",
+            name: "TVDatePicker",
             dependencies: []),
         .testTarget(
-            name: "TVDatePickerTestsDev",
-            dependencies: ["TVDatePickerDev"]),
+            name: "TVDatePickerTests",
+            dependencies: ["TVDatePicker"]),
     ]
 )
